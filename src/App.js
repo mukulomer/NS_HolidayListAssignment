7c83cb116828137e0f980103eb311142533c3f46
Mukul Omer <55843646+mukulomer@users.noreply.github.com>
2020-10-21 14:09:04 +0530

    1st change

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ export default function App() {
   const cities = ["Goa", "Darjeeling", "Lonavala"];
 
   return (
-    <div>
+    <>
       <ol>
         {cities.map((city, index) => (
-          <li key={`location ${index + 1}`}>{city}</li>
+          <li key={`location${index + 1}`}>{city}</li>
         ))}
       </ol>
-    </div>
+    </>
   );
 }
